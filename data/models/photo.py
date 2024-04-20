c997114a06852737e1b4b6a1bffd2e680bfed350
Nitor112221 <kostya1234moi@gmail.com>
2024-04-21 00:38:05 +0500

    удалено не нужное поле в моделе photo

--- data/models/photo.py
+++ data/models/photo.py
@@ -9,4 +9,3 @@ class Photo(SqlAlchemyBase):
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     id_template = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('template.id'))
-    name_photo = sqlalchemy.Column(sqlalchemy.String)
