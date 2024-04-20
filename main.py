53dbe9a01835b79b17c48a3dec66575c19755a34
Nitor112221 <kostya1234moi@gmail.com>
2024-04-20 15:20:07 +0500

    создан обработчик команды /help выводящий информацию о доступных командах

--- main.py
+++ main.py
@@ -19,11 +19,27 @@ async def start(update, context):
                                     f'моих возможностях напишите команду /help')
 
 
+async def help_command(update, context):
+    await update.message.reply_text(f'Список доступных команд и их описание:\n'
+                                    f'/start - запускает бота и выводит краткое описание его функционала\n'
+                                    f'/help - выводит данный список команд\n'
+                                    f'/change - меняет выбранный шаблон\n'
+                                    f'/create_sample - создаёт новый пользовательский шаблон\n'
+                                    f'/add_photo - включает режим добавления фото в шаблоны, чтобы выйти из него '
+                                    f'напишите команду /stop_add_photo\n'
+                                    f'/stop_add_photo - выключает режим добавления фото в шаблоны\n'
+                                    f'/support - активирует диалог с поддержкой\n'
+                                    f'Чтобы сделать стикерпак просто отправьте фотографию с лицом, и подождите пока бот'
+                                    f' его обработает и применит выбранный вами шаблон '
+                                    f'(убедитесь что режим добавления фото в шаблоны выключен)\n'
+                                    f'Приятного пользование ботом')
+
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
-    text_handler = CommandHandler('start', start)
 
-    application.add_handler(text_handler)
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('help', help_command))
 
     application.run_polling()
 
