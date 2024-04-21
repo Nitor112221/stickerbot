57e5b336fa5744c01d0c15a5d8ec59db62921828
Nitor112221 <kostya1234moi@gmail.com>
2024-04-21 20:29:34 +0500

    изменён конфиг

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "6970880601:AAFrg2Rc8oOrzaFM4yedFObk0D67JqGJZmw"  # ваш токен бота
+BOT_TOKEN = "введите ваш токен"  # ваш токен бота
 IMG_FOLDER = 'bot/img'
-ADMIN_ID = 5685707883
+ADMIN_ID = 5685707883  # id админа
